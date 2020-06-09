0739a7b4a0b138552089f7ae635db9f04929d7c7
lz-2019 <rick.lizhan@gmail.com>
2020-06-09 22:55:58 +0800

    add WriteEndHead

--- example/demo.go
+++ example/demo.go
@@ -12,7 +12,7 @@ func main() {
 	doc.WriteHead()
 	doc.WriteTitle(report.NewText("测试文档"))
 
-	doc.WriteTitle3(report.NewText("                               ———Web应用扫描"))
+	doc.WriteTitle3(report.NewText("                                        编号：2121234343"))
 	tableHead := [][]interface{}{
 		{report.NewText("部门或型号")},
 		{report.NewText("部门:研发;型号:martin;")},
